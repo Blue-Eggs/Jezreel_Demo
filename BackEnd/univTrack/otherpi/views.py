d9581b288473e1abda2df948ff0ab74f99776541
Corey Nambu <corey.nambu@gmail.com>
2022-10-25 22:22:23 -0700

    Moved sensitive files, integrated front/backend communication

--- BackEnd/univTrack/otherpi/views.py
+++ BackEnd/univTrack/otherpi/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 from . import trackInfo
 from .models import userTrack
 
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+
 def getName(request):
     all_info = userTrack.objects.all
     #print(all_info)
@@ -11,6 +15,8 @@ def getName(request):
 
    # return JsonResponse(all_info, safe = False)
 
+@api_view(['GET'],)
+@permission_classes([AllowAny],)
 def getTrackInfo(request):
     result = trackInfo.getInfoTrack('SHIPPO_TRANSIT')
     return JsonResponse(result, safe=False)
