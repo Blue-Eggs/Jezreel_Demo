e8c4385603e25e24bd06df5365cf96bbaca7034b
Jezreel Salinas <jsalinas@cpp.edu>
2022-11-15 13:20:02 -0800

    Added error handling for valid tracking id token but does not exist in the database

--- BackEnd/univTrack/otherpi/trackInfo.py
+++ BackEnd/univTrack/otherpi/trackInfo.py
@@ -1,4 +1,5 @@
 import shippo
+from . import serviceTrack
 
 #  test key
 shippo.config.api_key = 'shippo_test_a23d47137308fe441ddcde413467c8f2e8ed742a'
@@ -8,9 +9,17 @@ shippo.config.rates_req_timeout = 30.0
 shippo.config.timeout_in_seconds = None 
 
 def getInfoTrack(tracking_number):
-    carrier_token = 'shippo'
-    shippo.Parcel.
-    return shippo.Track.get_status(carrier_token, tracking_number)
+    carrier_token = serviceTrack.get_service(tracking_number)
+    if(carrier_token == None): 
+        print('Tracking ID error, returning shippo')
+        return shippo.Track.get_status('shippo', 'SHIPPO_TRANSIT')
+    
+    try:
+        data = shippo.Track.get_status(carrier_token, tracking_number)
+        return data
+    except:
+        print('Tracking ID does not exist in the data base')
+        return shippo.Track.get_status('shippo', 'SHIPPO_TRANSIT')
 
 
 #tracking_number = "SHIPPO_TRANSIT" #'9102969010383081813033'
--- BackEnd/univTrack/otherpi/views.py
+++ BackEnd/univTrack/otherpi/views.py
@@ -22,8 +22,8 @@ def getName(request):
 @permission_classes([AllowAny],)
 def testPOST(request): 
     data = json.loads(request.body) # b'{'title' : 'SHIPPO_TRANSIT', 'title2' : 'id'}
-    print(data['title'])
-    print(data['title2'])
+    print(data['title']) # Fixed Tracking ID
+    print(data['title2']) # ID passed
     result = trackInfo.getInfoTrack(data['title2'])
     return JsonResponse(result, safe=False)
 
