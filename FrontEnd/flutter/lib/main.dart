bee42f801bf9f2d7af50f28701c4645f5092ce95
Corey Nambu <corey.nambu@gmail.com>
2022-11-24 19:34:49 -0800

    Cleaned up json display, added more tracking info
    Also added back button

--- FrontEnd/flutter/lib/main.dart
+++ FrontEnd/flutter/lib/main.dart
@@ -86,55 +86,6 @@ class Home extends StatelessWidget {
             ),
             backgroundColor: Colors.purple[700],
           ),
-          drawer: Drawer(
-              backgroundColor: Colors.purple[700],
-              child: ListView(
-                children: <Widget>[
-                  UserAccountsDrawerHeader(
-                    accountName: Text("Individual Person"),
-                    accountEmail: Text("example@BlueEggs.com"),
-                    currentAccountPicture: GestureDetector(
-                        onTap: () => print("Account clicked"),
-                        child: CircleAvatar(
-                          backgroundImage: NetworkImage(mainProfilePicture),
-                        )),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/truckDrivingDown.jpg"),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                      title: Text("Home"),
-                      trailing: Icon(Icons.home),
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const Home(title: 'Home');
-                        }));
-                      }),
-                  ListTile(
-                      title: Text("View Packages"),
-                      trailing: Icon(Icons.backpack),
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const Packages(title: 'Packages');
-                        }));
-                      }),
-                  Divider(),
-                  ListTile(
-                      title: Text("Account"),
-                      trailing: Icon(Icons.account_box),
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const Account(title: 'Account');
-                        }));
-                      }),
-                ],
-              )),
           body: SingleChildScrollView(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -216,259 +167,17 @@ class Home extends StatelessWidget {
                         ),
                       ],
                     ),
-                    Container(child: Text("Images from:")),
+                    /*Container(child: Text("Images from:")),
                     Text("Adobe Stock: Profile Image\n"
                         "pexels.com: Package Handoff\n"
                         "iStock: Truck driving down road\n"
-                        "Images should be liscensed or removed"),
+                        "Images should be liscensed or removed"),*/
                   ],
                 ),
               ],
             ),
           )),
     );
-  }
-}
-
-class Packages extends StatelessWidget {
-  const Packages({Key? key, required this.title}) : super(key: key);
-  final String title;
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: InkWell(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return const Home(title: 'Home');
-                }));
-              },
-              child: Text(
-                "Universal Package Tracker",
-                style: TextStyle(
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            backgroundColor: Colors.purple[700],
-          ),
-          drawer: Drawer(
-              backgroundColor: Colors.purple[700],
-              child: ListView(
-                children: <Widget>[
-                  UserAccountsDrawerHeader(
-                    accountName: Text("Individual Person"),
-                    accountEmail: Text("example@BlueEggs.com"),
-                    currentAccountPicture: new GestureDetector(
-                        onTap: () => print("Account clicked"),
-                        child: const CircleAvatar()),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/truckDrivingDown.jpg"),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                      title: Text("Home"),
-                      trailing: Icon(Icons.home),
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const Home(title: 'Home');
-                        }));
-                      }),
-                  ListTile(
-                      title: Text("View Packages"),
-                      trailing: Icon(Icons.backpack),
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const Packages(title: 'Packages');
-                        }));
-                      }),
-                  Divider(),
-                  ListTile(
-                      title: Text("Account"),
-                      trailing: Icon(Icons.account_box),
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const Account(title: 'Account');
-                        }));
-                      }),
-                ],
-              )),
-          body: SingleChildScrollView(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Column(
-                  children: [
-                    /*TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter a search term',
-                      ),
-                    ),*/
-                    Text('Package Information',
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold)),
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Text('Tracking Number: '),
-                            Text('Destination: '),
-                            Text('Being Shipped From: '),
-                            Text('Being Shipped By: '),
-                            Text('Days in Transit: '),
-                            Text('Estimated Delivery: '),
-                            Row(children: [
-                              Text('Save Tracking Info'),
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(Icons.save),
-                              )
-                            ]),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          )),
-    );
-  }
-}
-
-class Account extends StatelessWidget {
-  const Account({Key? key, required this.title}) : super(key: key);
-  final String title;
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: InkWell(
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return const Home(title: 'Home');
-            }));
-          },
-          child: Text(
-            "Universal Package Tracker",
-            style: TextStyle(
-              color: Colors.black,
-            ),
-          ),
-        ),
-        backgroundColor: Colors.purple[700],
-      ),
-      drawer: Drawer(
-          backgroundColor: Colors.purple[700],
-          child: ListView(
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                accountName: Text("Individual Person"),
-                accountEmail: Text("example@BlueEggs.com"),
-                currentAccountPicture: new GestureDetector(
-                    onTap: () => print("Account clicked"),
-                    child: const CircleAvatar()),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/truckDrivingDown.jpg"),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-              ListTile(
-                  title: Text("Home"),
-                  trailing: Icon(Icons.home),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const Home(title: 'Home');
-                    }));
-                  }),
-              ListTile(
-                  title: Text("View Packages"),
-                  trailing: Icon(Icons.backpack),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const Packages(title: 'Packages');
-                    }));
-                  }),
-              Divider(),
-              ListTile(
-                  title: Text("Account"),
-                  trailing: Icon(Icons.account_box),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const Account(title: 'Account');
-                    }));
-                  }),
-            ],
-          )),
-      body: SingleChildScrollView(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Column(
-              children: [
-                /*TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter a search term',
-                      ),
-                    ),*/
-                Text('Account Information',
-                    style:
-                        TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                Row(
-                  children: [
-                    Row(children: [
-                      Column(
-                        children: [
-                          Text('Name: '),
-                          Text('Email: '),
-                          Text('Phone Number: '),
-                          Text('Packages Being Tracked'),
-                        ],
-                      ),
-                      Column(children: [
-                        Text('Individual Person'),
-                        Text('example@BlueEggs.com'),
-                        Text('(123)456-7890'),
-                        Text(''),
-                        //Row(
-                        //children: [
-                        //Text('Save Account Information'),
-                        //IconButton(
-                        //onPressed: () {},
-                        //icon: Icon(Icons.save),
-                        //)
-                      ]),
-                    ])
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    ));
   }
 }
 
@@ -478,18 +187,92 @@ class TrackInformation extends StatelessWidget {
   String title;
   String trackhistory;
   TrackInfo trackinfo;
-  
+
   ///
   @override
   Widget build(BuildContext context) {
-    
+
 
     return Center(
-      child: GestureDetector(
-          child: Text(trackinfo.address_from?.city ?? 'null'),
-          onTap: () {
-            Navigator.pop(context, true);
-          }),
-    );
+      child: DefaultTextStyle(
+        style: TextStyle(),
+        child: Row(
+          children: [
+            Column( //First column, has the back button
+              children: [
+                Material(
+                  child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context, true);
+                      },
+                      icon: Icon(Icons.arrow_back_rounded)
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 170,
+              child: Column( //Second column, has the labels for the data
+                  children: [
+                    Text("Tracking number: ",
+                    style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("Carrier: ",
+                      style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("Shipping category: ",
+                      style: TextStyle(fontWeight: FontWeight.bold),),
+
+                  ]),
+            ),
+            SizedBox(
+              width: 200,
+              child: Column( //Third Column, holds data from the json
+                  children: [
+                    Text(trackinfo.tracking_number ?? 'null'),
+                    Text(trackinfo.carrier ?? 'null'),
+                    Text(trackinfo.servicelevel.name ?? 'null'),
+
+                  ]),
+            ),
+            SizedBox(
+              child: DefaultTextStyle(
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+                ),
+                child: Column(
+                  children: [
+                    Text("Current eta: ",
+                      style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("Status updated on: ",
+                      style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("Package status: ",
+                      style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("Current state: ",
+                      style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("Current city: ",
+                      style: TextStyle(fontWeight: FontWeight.bold),),
+                  ]
+                ),
+              ),
+            ),
+            SizedBox(
+              child: DefaultTextStyle(
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+                child: Column(
+                    children: [
+                      Text(trackinfo.eta ?? 'null'),
+                      Text(trackinfo.tracking_status?.status_date ?? 'null'),
+                      Text(trackinfo.tracking_status?.status_details ?? 'null'),
+                      Text(trackinfo.tracking_status?.location?.state ?? 'null'),
+                      Text(trackinfo.tracking_status?.location?.city ?? 'null'),
+                    ]
+                ),
+              ),
+            ),
+          ]),
+      ),
+      );
   }
 }
