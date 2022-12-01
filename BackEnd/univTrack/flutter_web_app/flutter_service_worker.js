38e4e2736a5b09046dc7c2f9fa96d28919027fc2
Corey Nambu <corey.nambu@gmail.com>
2022-11-30 20:21:18 -0800

    Added support for domain in frontend

--- BackEnd/univTrack/flutter_web_app/flutter_service_worker.js
+++ BackEnd/univTrack/flutter_web_app/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "13c9ac589171f4e3504a72f1a2f37ac9",
-"/": "13c9ac589171f4e3504a72f1a2f37ac9",
-"main.dart.js": "640509645e56cf4eaf9887c3f1b847e8",
+"index.html": "5ed6e2044f63c37a674304ed97db47e9",
+"/": "5ed6e2044f63c37a674304ed97db47e9",
+"main.dart.js": "be9823f99ce8d6b4883b47c0fd619e7b",
 "manifest.json": "c991665da2bcde6b3e0bde328fb30aff",
 "version.json": "68da5c52cc4da931cba771792b7ea782"
 };
